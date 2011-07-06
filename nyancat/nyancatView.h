edbd22920dd7f1ba1e9a9c4512c4b5ec4356bc4a
Vinay Tota <vinaytota@Vinay-Totas-MacBook-Air.local>
2011-07-05 20:04:18 -0700

    Initial Commit

---
+++ nyancat/nyancatView.h
@@ -0,0 +1,17 @@
+//
+//  nyancatView.h
+//  nyancat
+//
+//  Created by Vinay Tota on 7/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+
+@interface nyancatView : ScreenSaverView {
+@private
+    
+}
+
+@end
---
+++ nyancat/nyancatView.m
@@ -0,0 +1,53 @@
+//
+//  nyancatView.m
+//  nyancat
+//
+//  Created by Vinay Tota on 7/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "nyancatView.h"
+
+
+@implementation nyancatView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
